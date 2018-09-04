a97626e6f5f4598bfae0a13a1f4b97aa7f519e8d
Torarinn gunnarsson <torarinngunnarsson@Torarinns-MacBook-Pro.local>
2018-09-04 10:32:31 +0000

    DEletadi bad code

--- max_int.py
+++ max_int.py
@@ -2,9 +2,7 @@
 #1, 2, 3, 6, 11, 20, 37, _, _, _, …
 
 num_int = int(input("Input a number: "))    # Do not change this line
-num_int = i
-while(i<-1):
-    print("Please enter a number", i ,"when finished, enter a negative number")
+
 # Fill in the missing code
 print("The maximum is", max_int)    # Do not change this line
 
