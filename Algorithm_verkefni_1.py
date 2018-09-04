6febffe3db594401e9a0d0892597c2a9d41c44be
Torarinn gunnarsson <torarinngunnarsson@Torarinns-MacBook-Pro.local>
2018-09-04 10:22:53 +0000

    HEI

---
+++ Algorithm_verkefni_1.py
@@ -0,0 +1,11 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+num_int = 0
+count = 0
+
+if num_int >= 0:
+    count += 1
+    print(count)
+elif num_int < 0:
+    print("Integer can not be positive")
+        # Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
---
+++ max_int.py
@@ -0,0 +1,11 @@
+#Design an algorithm that generates the first n numbers in the following sequence:; 
+#1, 2, 3, 6, 11, 20, 37, _, _, _, …
+
+num_int = int(input("Input a number: "))    # Do not change this line
+num_int = i
+while(i<-1):
+    print("Please enter a number", i ,"when finished, enter a negative number")
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
+
+
